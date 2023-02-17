5c12379b831232df404cdbb3c59c1303223e32d3
marcello de bonis <marcello98.videogames@gmail.com>
2023-02-17 11:16:29 +0100

    Testing

---
+++ RogueLike-PB/Assets/Scripts/ButtonLibrary/ButtonLibrary.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonLibrary : Singleton<ButtonLibrary>
+{
+
+#region Variables & Properties
+
+
+
+#endregion
+
+#region Methods
+
+public void OpenGame()
+{
+   SceneManager.LoadScene("MapScene");
+}
+2
+
+public void ExitGame()
+{
+    
+}
+
+#endregion
+
+}
