ce60643c3544a4f973a35a664d4d584378f6c2d2
marcello de bonis <marcello98.videogames@gmail.com>
2023-03-03 12:34:35 +0100

    Updated Other object to be spawned

--- RogueLike-PB/Assets/Scripts/GameObjectRoomStruct/GameObjectRoomStruct.cs
+++ RogueLike-PB/Assets/Scripts/GameObjectRoomStruct/GameObjectRoomStruct.cs
@@ -2,7 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameObjectRoomStruct : MonoBehaviour
+[System.Serializable]
+public class GameObjectRoomStruct
 {
 
 #region Variables & Properties
